3d38017d8412ea81e73cced59481522c3f36fa0a
kochetkov <s.kochetkov@micord.ru>
2025-09-12 14:01:55 +0300

    fix

--- kie-soup-maven-utils/kie-soup-maven-integration/src/main/java/org/appformer/maven/integration/embedder/MavenProjectLoader.java
+++ kie-soup-maven-utils/kie-soup-maven-integration/src/main/java/org/appformer/maven/integration/embedder/MavenProjectLoader.java
@@ -38,7 +38,7 @@ import org.slf4j.LoggerFactory;
 public class MavenProjectLoader {
 
     public static final String FORCE_OFFLINE = "kie.maven.offline.force";
-    protected static boolean IS_FORCE_OFFLINE = Boolean.valueOf(System.getProperty(FORCE_OFFLINE, "false"));
+    protected static boolean IS_FORCE_OFFLINE = Boolean.parseBoolean(System.getProperty(FORCE_OFFLINE, "false"));
 
     private static final Logger log = LoggerFactory.getLogger(MavenProjectLoader.class);
 
@@ -183,7 +183,7 @@ public class MavenProjectLoader {
         }
         return mavenProject;
     }
-    
+
     public static boolean isOffline() {
         return IS_FORCE_OFFLINE;
     }
